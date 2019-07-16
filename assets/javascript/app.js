00bf7d394d3652b678b797c8790e43b362fc8c9f
Anand Chaudhary <anand92490@gmail.com>
2019-07-16 01:32:53 -0500

    saving changes.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -50,7 +50,9 @@ function nextQuestion(){
     var isQuestionOver = (quizQuestions.length - 1) === currentQuestion;
 
     if (isQuestionOver){
-        console.log('Game is over');
+        console.log('Game over');
+        displayResult();
+     
     }else{
        
     currentQuestion++;
@@ -87,8 +89,12 @@ function timeUp(){
       var question =   quizQuestions[currentQuestion].questions;
       var choices =   quizQuestions[currentQuestion].choices;
     $("#time").html('Time Remaining :' + counter);
-      $("#game").html(`<h4>${question}</h4> ${loadChoices(choices)}`);
+      $('#game').html(`<h4>${question}</h4> ${loadChoices(choices)}
+      
+      `);
     }
+
+
 
     function loadChoices(choices){
          var result = '';
@@ -101,16 +107,50 @@ function timeUp(){
     }
 
 $(document).on('click', '.choice', function (){
+    clearInterval(timer);
     var selectedAnswer = $(this).attr('data-answer');
     var correctAnswer = quizQuestions[currentQuestion].correctAnswer;
 
     if ( correctAnswer === selectedAnswer){
         score++;
-        console,log('win');
+        nextQuestion();
+        console.log('win');
     }else{
         lost++
         console.log("loss");
-    }
+        nextQuestion();
+    } 
 });
+
+function displayResult() {
+    var result = `
+    <p>You got ${score} answer correct</p> 
+    <p>You got ${lost} wrong </p> 
+    <p> Out of ${quizQuestions.length} total questions!!</p> 
+    <button class = "btn btn-warning" id="reset"> Play Again!</button>
+    `;
+
+
+    $('#game').html(result);
+}
+
+$(document).on('click', '#reset', function(){ 
+
+   counter = 5;
+   currentQuestion = 0;
+   score = 0;
+   lost = 0;
+   timer = null;
+
+   loadquestion();
+});
+
+function loadRemainingQuestion(){
+    var remainingQuestion = quizQuestions.length - (currentQuestion + 1);
+    var totalQuestion = quizQuestions.length;
+
+    return  `remaining Questiomn : ${remainingQuestion} / ${totalQuestion}`;
+}
+
     loadquestion(); 
   
